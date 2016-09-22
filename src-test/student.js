3e37702d8a69ae2d748ab1d8dbed349c3e1b26be
Jack <jack.willis-craig@hotmail.com>
2016-09-22 09:11:11 +0000

    Adding endpoint for registering a student.

--- src-test/student.js
+++ src-test/student.js
@@ -5,15 +5,42 @@ import UtsHelpsApi from '../dist/UtsHelpsApi'
 const api = new UtsHelpsApi()
 
 describe('Students', () => {
-  describe('#getStudents()', function() {
+  describe('#getStudent()', function() {
     this.timeout(7000)
 
     it('should return information about a student given an ID', done => {
-      const studentId = '11692450'
+      const studentId = '11692946'
       api.getStudent(studentId, (err, res) => {
         assert.isNull(err)
         assert.isTrue(res.IsSuccess)
         assert.isObject(res.Result)
+        console.log(res.Result)
+        done()
+      })
+    })
+  })
+
+  describe('#registerStudent()', function() {
+    this.timeout(7000)
+
+    it('should success if the student was created', done => {
+      const details = {
+        studentId: '11692946',
+        dateOfBirth: '6 June 1994',
+        degree: 'UG',
+        status: 'Local',
+        firstLanguage: 'English',
+        countryOrigin: 'Australia',
+        creatorId: '1'
+      }
+      api.registerStudent(details, (err, res) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(res)
+        }
+        assert.isNull(err)
+        assert.isTrue(res.IsSuccess)
         done()
       })
     })
--- src/components/student.js
+++ src/components/student.js
@@ -8,6 +8,51 @@ export default {
   methods: {
     getStudent(id, callback) {
       this.get(this.endpoints.student.get.replace('{id}', id), callback)
+    },
+
+    registerStudent(details, callback) {
+      const keyMap = {
+        studentId: 'StudentId',
+        dateOfBirth: 'DateOfBirth',
+        gender: 'Gender',
+        degree: 'Degree',
+        status: 'Status',
+        firstLanguage: 'FirstLanguage',
+        countryOrigin: 'CountryOrigin',
+        background: 'Background',
+        degreeDetails: 'DegreeDetails',
+        altContact: 'AltContact',
+        preferredName: 'PreferredName',
+        hsc: 'HSC',
+        hscMark: 'HSCMark',
+        ielts: 'IELTS',
+        ieltsMark: 'IELTSMark',
+        toefl: 'TOEFL',
+        toeflMark: 'TOEFLMark',
+        tafe: 'TAFE',
+        tafeMark: 'TAFEMark',
+        cult: 'CULT',
+        cultMark: 'CULTMark',
+        insearchDeep: 'InsearchDEEP',
+        insearchDeepMark: 'InsearchDEEPMark',
+        insearchDiploma: 'InsearchDiploma',
+        insearchDiplomaMark: 'InsearchDiplomaMark',
+        foundationCourse: 'FoundationCourse',
+        foundationCourseMark: 'FoundationCourseMark',
+        creatorId: 'CreatorId'
+      }
+      const newKeys = {}
+      Object.keys(details).forEach(key => {
+        const mappedKey = keyMap[key]
+        if (keyMap[key]) {
+          newKeys[mappedKey] = details[key]
+        } else {
+          newKeys[key] = details[key]
+        }
+      })
+      this.post(this.endpoints.student.register, {
+        form: details
+      }, callback)
     }
   }
 }
